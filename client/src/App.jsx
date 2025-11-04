110d6a98fe50a82b1a8f10e29d13c09d36bbf7a2
Beau <77253127+ieBeau@users.noreply.github.com>
2025-11-04 12:09:49 -0500

    Init commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <img src="./src/assets/Family_Feud_Logo.png" alt="Family Feud Logo" className="logo" />
+      <img src="./Family_Feud_Logo.png" alt="Family Feud Logo" className="logo" />
       <h1>Welcome to Family Feud!</h1>
       <p>This is the main application component.</p>
     </>
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  let environment = 'development';
+
+  if (command === 'serve') environment = 'development';
+  else environment = 'production';
+
+  return {
+    plugins: [react()],
+    preview: {
+      allowedHosts: [
+        'familyfeud-client.onrender.com'
+      ]
+    },
+    server: {
+      proxy: {
+        '/api': environment === "development"
+          ? `http://localhost:3000`
+          : 'https://familyfeud-server.onrender.com'
+      }
+    }
+  }
 })
