4d93fccaf8387327b05b9354237c87c2602dafdd
Beau <77253127+ieBeau@users.noreply.github.com>
2025-11-14 22:31:57 -0500

    update: remove authentication middleware from GET and POST question routes

--- server/routes/v1/question.route.js
+++ server/routes/v1/question.route.js
@@ -6,9 +6,9 @@ import questionController from '../../controllers/question.controller.js';
 const router = express.Router();
 
 // Protected routes
-router.get('/', authMiddleware.requireSignin, questionController.getRandomQuestion);
-router.get('/:id', authMiddleware.requireSignin, questionController.getQuestion);
-router.post('/', authMiddleware.requireSignin, questionController.createQuestion);
+router.get('/', questionController.getRandomQuestion);
+router.get('/:id', questionController.getQuestion);
+router.post('/', questionController.createQuestion);
 
 // Authorization middleware to ensure user can only modify their own questions
 router.put('/:id', authMiddleware.requireSignin, authMiddleware.hasAuthorization, questionController.updateQuestion);
