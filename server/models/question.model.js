a58a57635188a801e9c942c32a657a052b4813c6
Beau <77253127+ieBeau@users.noreply.github.com>
2025-11-21 00:00:11 -0500

    refactor: reorder QuestionSchema definition for improved clarity

--- server/models/question.model.js
+++ server/models/question.model.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-const QuestionSchema = new mongoose.Schema({
-    question: { type: String, trim: true, required: "Question is required" },
-    answers: [ AnswerSchema ]
-});
-
 const AnswerSchema = new mongoose.Schema({
     answer: { type: String, required: true, trim: true }, 
     points: { type: Number, required: true }
+});
+
+const QuestionSchema = new mongoose.Schema({
+    question: { type: String, trim: true, required: "Question is required" },
+    answers: [ AnswerSchema ]
 });
 
 export default mongoose.model('Question', QuestionSchema)
